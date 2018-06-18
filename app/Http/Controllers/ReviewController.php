4d44424a0ba0669074227394d5c4a34e5dd269cd
l0oktaan <looktaan.tc3@gmail.com>
2018-06-18 22:29:52 +0700

    Created Review Resource

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -16,9 +16,10 @@ class ReviewController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Product $product)
     {
-        //
+        //return $product->reviews;
+        return ReviewResource::collection($product->reviews);
     }
 
     /**
